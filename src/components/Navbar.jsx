b53c1f4621fd325547e0276b4c3561b0bdd740fc
trongtinIUH <trantin1973@gmail.com>
2025-06-03 00:13:39 +0700

    thêm projects 4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
             <li><a href="#about">ABOUT US</a></li>
             <li><a href="#skills">SKILLS</a></li>
             <li><a href="#experience">EXPERIENCE</a></li>
-            <li><a href="#projects">PROJECTS</a></li>
+            <li><a href="#">PORTFOLIO</a></li>
             <li><a href="#">CONTACT US</a></li>
-            <li><a href="/mycv.pdf" target="_blank" >MY CV</a></li>
+            <li><a href="/Tran-Trong-Tin-TopCV.vn-050525.105126.pdf" target="_blank">MY CV</a></li>
       </ul>
     </nav>
   );
